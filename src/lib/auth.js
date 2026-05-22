9dc734675577a9d71ef637f28661a1f5bcd26edb
Omi <seyam.islam020@gmail.com>
2026-05-22 16:17:21 +0600

    fix the mongodb dns issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,16 +1,13 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('docAppoint');
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
-  }),
+  database: mongodbAdapter(db),
   baseURL: process.env.BETTER_AUTH_URL,
   trustedOrigins: [process.env.BETTER_AUTH_URL, "https://doc-appoinment-coral.vercel.app"],
    emailAndPassword: {    
