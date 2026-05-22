b3b3b0848c600c4828b1ab3c4bf9ad509b2d379a
Omi <seyam.islam020@gmail.com>
2026-05-22 15:56:24 +0600

    update the better auth url

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -11,6 +11,8 @@ export const auth = betterAuth({
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
+  baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [process.env.BETTER_AUTH_URL, "https://doc-appoinment-coral.vercel.app"],
    emailAndPassword: {    
         enabled: true
     },
